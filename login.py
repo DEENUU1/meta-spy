8f05bb44bdf9e7202a89bbac9a4accec8e0c982e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-23 19:36:00 +0200

    Add function to close cookie term modal

--- login.py
+++ login.py
@@ -27,3 +27,16 @@ chrome_options.add_argument("--profile-directory=Default")
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(FACEBOOK_BASE_URL)
+
+COOKIE_TERM_CSS_SELECTOR = "._42ft._4jy0._al65._4jy3._4jy1.selected._51sy"
+INPUT_TEXT_CSS_SELECTOR = "//input[@type='text']"
+PASSWORD_CSS_SELECTOR = "//input[@placeholder='Hasło']"
+SUBMIT_BUTTON_SELECTOR = "//button[@type='submit']"
+wait = WebDriverWait(driver, 10)
+
+def close_cookie_term(cookie_css_selector: str) -> None:
+    button = driver.find_element(By.CSS_SELECTOR, cookie_css_selector)
+    button.click()
+
+
+
