2cbd3fa86f94829244e05d972ad3e8bf28fe0b92
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-24 19:10:43 +0200

    Add function to change Queue object status to True (#4)

--- src/repository.py
+++ src/repository.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from sqlalchemy.orm import Session
 from models import Queue
 
@@ -8,4 +10,16 @@ def create_queue(db: Session, url: str) -> Queue:
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
+    return db_item
+
+
+def update_queue_status_to_true(db: Session, queue_id: int) -> Type[Queue]:
+    """Update queue status to True"""
+    db_item = db.query(Queue).filter(Queue.id == queue_id).first()
+    if db_item:
+        db_item.status = True
+        db.commit()
+        db.refresh(db_item)
+    else:
+        raise Exception("Queue not found")
     return db_item
