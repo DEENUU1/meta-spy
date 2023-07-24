5825e3f9acf2c3ea9ef50bf6c604845f0c520d58
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-24 03:15:02 +0200

    Add method load_cookies to load add cookies with login data (#2)

--- scraper.py
+++ scraper.py
@@ -38,13 +38,34 @@ class FacebookScraper:
         self.base_url = f"https://www.facebook.com/{user_id}/friends"
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver = self.driver
+        self.load_cookies()
         self.driver.get(self.base_url)
 
     def load_cookies(self) -> None:
         try:
-            with open("cookies.txt", "r") as file:
+            with open("cookies.json", "r") as file:
                 cookies = json.load(file)
                 for cookie in cookies:
                     self.driver.add_cookie(cookie)
         except Exception as e:
             logging.error(f"Error loading cookies: {e}")
+
+    def scroll_page(self) -> None:
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+        while True:
+            self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+            WebDriverWait(self.driver, 10).until(
+                lambda driver: driver.execute_script(
+                    "return document.body.scrollHeight"
+                )
+                > last_height
+            )
+            last_height = self.driver.execute_script(
+                "return document.body.scrollHeight"
+            )
+            if last_height == self.driver.execute_script(
+                "return document.body.scrollHeight"
+            ):
+                break
