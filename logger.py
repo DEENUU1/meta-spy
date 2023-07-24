46c5c2ff0acc2ed436f133aee25c38fc893e14e5
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-24 02:42:03 +0200

    Add logger configuration (#11)

---
+++ logger.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(
+    filename="logs.json",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
--- login.py
+++ login.py
@@ -21,31 +21,32 @@ chrome_options.add_argument("--disable-features=IsolateOrigins,site-per-process"
 chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
 chrome_options.add_argument("--profile-directory=Default")
 
-driver = webdriver.Chrome(options=chrome_options)
-
 
 class FacebookLogIn:
     """
     Log in to Facebook using email and password
     """
 
-    def __init__(self, driver: webdriver.Chrome = driver) -> None:
+    def __init__(self) -> None:
         self.email = os.getenv("FACEBOOK_EMAIL")
         self.password = os.getenv("FACEBOOK_PASSWORD")
         self.base_url = "https://www.facebook.com/"
-        self.driver = driver
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = self.driver
         self.driver.get(self.base_url)
         self.cookie_term_css_selector = "._42ft._4jy0._al65._4jy3._4jy1.selected._51sy"
         self.input_text_css_selector = "//input[@type='text']"
         self.password_css_selector = "//input[@placeholder='Hasło']"
         self.submit_button_selector = "//button[@type='submit']"
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(self.driver, 10)
 
     def close_cookie_term(self) -> None:
         """
         Close modal with cookie information
         """
-        button = driver.find_element(By.CSS_SELECTOR, self.cookie_term_css_selector)
+        button = self.driver.find_element(
+            By.CSS_SELECTOR, self.cookie_term_css_selector
+        )
         button.click()
 
     def facebook_login(self) -> None:
@@ -55,12 +56,12 @@ class FacebookLogIn:
         user_name = self.wait.until(
             EC.presence_of_element_located((By.XPATH, self.input_text_css_selector))
         )
-        password = driver.find_element(By.XPATH, self.password_css_selector)
+        password = self.driver.find_element(By.XPATH, self.password_css_selector)
 
         user_name.send_keys(self.email)
         password.send_keys(self.password)
 
-        log_in_button = driver.find_element(By.XPATH, self.submit_button_selector)
+        log_in_button = self.driver.find_element(By.XPATH, self.submit_button_selector)
         log_in_button.click()
 
         self.wait.until(
@@ -71,14 +72,14 @@ class FacebookLogIn:
         """
         Add security code for 2-step verification of email and password
         """
-        security_code_input = driver.find_elements(
+        security_code_input = self.driver.find_elements(
             By.XPATH, self.input_text_css_selector
         )
         if security_code_input:
             security_code = input("Enter your security code: ")
             security_code_input[0].send_keys(security_code)
 
-        save_button = driver.find_element(By.XPATH, self.submit_button_selector)
+        save_button = self.driver.find_element(By.XPATH, self.submit_button_selector)
         save_button.click()
 
     def save_browser(self) -> None:
@@ -88,15 +89,16 @@ class FacebookLogIn:
         self.wait.until(
             EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Facebook']"))
         )
-        continue_button = driver.find_element(By.XPATH, self.submit_button_selector)
+        continue_button = self.driver.find_element(
+            By.XPATH, self.submit_button_selector
+        )
         continue_button.click()
 
-    @staticmethod
-    def save_cookies() -> None:
+    def save_cookies(self) -> None:
         """
         Save cookies with log in account to json file
         """
-        cookies = driver.get_cookies()
+        cookies = self.driver.get_cookies()
         with open("cookies.json", "wb") as file:
             pickle.dump(cookies, file)
 
