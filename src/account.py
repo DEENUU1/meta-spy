21eecdb00d1e473cdfdb9b6ab8d5d957250ae5ea
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-26 22:00:05 +0200

    Add facebookbasescraper class to avoid duplicates (#25)

--- src/account.py
+++ src/account.py
@@ -29,6 +29,32 @@ logging.basicConfig(
 models.Base.metadata.create_all(database.engine)
 
 
+class FacebookBaseScraper(Scraper):
+    def __init__(self, user_id) -> None:
+        super().__init__()
+        self._user_id = user_id
+        self._base_url = f"https://www.facebook.com/{self._user_id}"
+        self._driver = webdriver.Chrome(options=self._chrome_driver_configuration())
+        self._driver.get(self._base_url)
+        self._wait = WebDriverWait(self._driver, 10)
+
+    def _load_cookies(self) -> None:
+        """
+        Load cookies with a log in session
+        """
+        try:
+            self._driver.delete_all_cookies()
+            with open(Config.COOKIES_FILE_PATH, "rb") as file:
+                cookies = pickle.load(file)
+                for cookie in cookies:
+                    try:
+                        self._driver.add_cookie(cookie)
+                    except Exception as e:
+                        logging.error(f"Error adding cookie: {cookie}, Exception: {e}")
+        except Exception as e:
+            logging.error(f"Error loading cookies: {e}")
+
+
 class AccountScraper(Scraper):
     """
     Scrape user's personal information
