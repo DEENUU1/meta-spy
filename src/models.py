54dc2ddbd41e1e500deed602bc09f8127180428d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-27 14:09:30 +0200

    Add models (#61)
    Person
    Friends
    Image
    Places
    WorkAndEducation
    FamilyMember

--- src/models.py
+++ src/models.py
@@ -1,23 +1,60 @@
-from sqlalchemy import Column, String
-from sqlalchemy import Boolean, Integer, DateTime
+from sqlalchemy import Column, String, Integer, ForeignKey
 from database import Base
-import datetime
-from pydantic import BaseModel
+from sqlalchemy.orm import relationship
 
 
-class Queue(Base):
-    __tablename__ = "queue"
-
+class Person(Base):
+    __tablename__ = "persons"
     id = Column(Integer, primary_key=True, index=True)
-    url = Column(String, unique=True, index=True)
-    datetime = Column(DateTime, default=datetime.datetime.utcnow)
-    done = Column(Boolean, default=False)
+    full_name = Column(String, index=True)
+    url = Column(String, index=True)
+    facebook_id = Column(String, index=True)
+    family_member = relationship("FamilyMember", back_populates="person", uselist=False)
+    places = relationship("Places", back_populates="person")
+    work_and_education = relationship("WorkAndEducation", back_populates="person")
+    image = relationship("Image", back_populates="person", uselist=False)
+    friends = relationship("Friends", back_populates="person")
 
 
-class QueueBase(BaseModel):
-    url: str
-    datetime: datetime.datetime
-    done: bool
+class Friends(Base):
+    __tablename__ = "friends"
+    id = Column(Integer, primary_key=True, index=True)
+    person_id = Column(Integer, ForeignKey("persons.id"))
+    fullname = Column(String)
+    url = Column(String)
+    person = relationship("Person", back_populates="friends")
 
-    class Config:
-        from_attribute = True
+
+class Image(Base):
+    __tablename__ = "images"
+    id = Column(Integer, primary_key=True, index=True)
+    path = Column(String)
+    person_id = Column(Integer, ForeignKey("persons.id"))
+    person = relationship("Person", back_populates="image")
+
+
+class Places(Base):
+    __tablename__ = "places"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    date = Column(String)
+    person_id = Column(Integer, ForeignKey("persons.id"))
+    person = relationship("Person", back_populates="places")
+
+
+class WorkAndEducation(Base):
+    __tablename__ = "work_and_education"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    person_id = Column(Integer, ForeignKey("persons.id"))
+    person = relationship("Person", back_populates="work_and_education")
+
+
+class FamilyMember(Base):
+    __tablename__ = "family_members"
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    role = Column(String)
+    url = Column(String)
+    person_id = Column(Integer, ForeignKey("persons.id"))
+    person = relationship("Person", back_populates="family_member")
