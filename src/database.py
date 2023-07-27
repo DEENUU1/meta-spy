bb5e90d30b56496d8eecdddfe178da2fc796bf7d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-27 15:39:30 +0200

    Refactor scrapers to move every class to other file (#61)

--- src/database.py
+++ src/database.py
@@ -2,7 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import Config
+from sqlalchemy.orm import Session
+from contextlib import contextmanager
+
 
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+@contextmanager
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
