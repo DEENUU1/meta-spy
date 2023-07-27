29f99609ed69d34e7ddc48ef2be28bfefe17ed00
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-27 19:31:45 +0200

    Add tests for models using pytest (#61)

--- src/database.py
+++ src/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import Config
 
 
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URL)
+engine = create_engine("sqlite:///database.db")
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
-from database import Base
+from .database import Base
 from sqlalchemy.orm import relationship
 
 
