f1321254b92e8f5ee5754d037e3fe32278e458fe
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-27 15:22:10 +0200

    Update schemas for existing models (#61)

--- src/schemas.py
+++ src/schemas.py
@@ -1,44 +1,35 @@
 from pydantic import BaseModel
-from typing import List
+from typing import Optional
+
+
+class FriendsSchema(BaseModel):
+    full_name: str
+    url: Optional[str] = None
 
 
 class ImageSchema(BaseModel):
     path: str
-    person_id: int
 
 
 class PlacesSchema(BaseModel):
     name: str
-    date: str
-    person_id: int
+    date: Optional[str] = None
 
 
 class WorkAndEducationSchema(BaseModel):
     name: str
-    person_id: int
 
 
 class FamilyMemberSchema(BaseModel):
     full_name: str
-    role: str
-    url: str
-    person_id: int
-
-
-class FriendsSchema(BaseModel):
-    fullname: str
-    url: str
+    role: Optional[str] = None
+    url: Optional[str] = None
 
 
 class PersonSchema(BaseModel):
     full_name: str
-    url: str
-    facebook_id: str
-    family_member: FamilyMemberSchema
-    places: List[PlacesSchema]
-    work_and_education: List[WorkAndEducationSchema]
-    image: ImageSchema
-    friends: List[FriendsSchema]
+    url: Optional[str] = None
+    facebook_id: Optional[str] = None
 
     class Config:
         orm_mode = True
