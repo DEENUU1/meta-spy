3bcda203eba2008e163232d1ed65347f0337b5ab
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-07-27 23:03:33 +0200

    Bugfix with relative imports (#64)
    Uncomment pytests for models

--- src/database.py
+++ src/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from models import Base
 
 engine = create_engine("sqlite:///database.db")
-Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
