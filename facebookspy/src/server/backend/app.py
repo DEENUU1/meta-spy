92ce3e8d112018c1d17fdfb97884f23dc3cb2698
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-01 23:58:31 +0200

    Add endpoint to return detailed person object based on facebook id (#117)

--- facebookspy/src/server/backend/app.py
+++ facebookspy/src/server/backend/app.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, Depends
 from typing import List, Annotated
 from ...schemas import PersonSchema
-from ...repository import (
-    get_people,
-)
+from ...repository import get_people, get_person
 
 app = FastAPI()
 
@@ -14,5 +12,11 @@ def home():
 
 
 @app.get("/people/")
-def get_people_list(people: Annotated[List[PersonSchema], Depends(get_people)]):
+async def get_people_list(people: Annotated[List[PersonSchema], Depends(get_people)]):
     return people
+
+
+@app.get("/people/{facebook_id}", response_model=PersonSchema)
+async def get_person_by_facebook_id(facebook_id: str):
+    person = await get_person(facebook_id)
+    return person
