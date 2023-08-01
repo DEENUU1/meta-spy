54dd0c4861fbdd332c6028958347da197d61b5fb
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-02 00:14:22 +0200

    Add function get_person without async to use it inside pipeline (#123)

--- facebookspy/src/server/backend/app.py
+++ facebookspy/src/server/backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from typing import List, Annotated
-from ...schemas import PersonSchema
-from ...repository import get_people, get_person
+from ...schemas import PersonSchema, ReviewsSchema
+from ...repository import get_people, get_person_by_facebook_id, get_reviews_list
 
 app = FastAPI()
 
@@ -18,5 +18,11 @@ async def get_people_list(people: Annotated[List[PersonSchema], Depends(get_peop
 
 @app.get("/people/{facebook_id}", response_model=PersonSchema)
 async def get_person_by_facebook_id(facebook_id: str):
-    person = await get_person(facebook_id)
+    person = await get_person_by_facebook_id(facebook_id)
     return person
+
+
+@app.get("/review/{person_id}", response_model=List[ReviewsSchema])
+async def get_reviews_by_person_id(person_id: int):
+    reviews = await get_reviews_list(person_id)
+    return reviews
