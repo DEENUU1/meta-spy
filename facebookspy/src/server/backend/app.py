1bea728a551ae17b40decf3819ad085f18faa125
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-03 23:38:26 +0200

    Add middleware for fastapi app (#147)

--- facebookspy/src/server/backend/app.py
+++ facebookspy/src/server/backend/app.py
@@ -26,8 +26,24 @@ from ...models import (
     FamilyMember,
 )
 from ...database import Session, get_session
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
