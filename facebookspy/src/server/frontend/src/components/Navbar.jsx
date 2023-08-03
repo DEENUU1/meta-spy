5d8d6f92e4edf505b41bdda94ad0e8d4021c1869
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-03 22:47:29 +0200

    Add navbar (#144)

---
+++ facebookspy/src/server/frontend/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+export default function Navbar() {
+    return (
+      <nav className="navigation">
+        <a href="/" className="brand-name">
+          MacroSoft
+        </a>
+        <button className="hamburger">
+          {/* icon from heroicons.com */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div
+          className="navigation-menu">
+          <ul>
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
