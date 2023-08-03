430aac75516f0f45e23955894f445e7cdc47ad8e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-03 12:41:48 +0200

    Change file structure for react app  (#141)

--- facebookspy/src/server/frontend/src/App.jsx
+++ facebookspy/src/server/frontend/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import HomePage from './components/HomePage';
+import DetailPage from './components/DetailPage';
+// import './styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="app-container">
+        <Sidebar />
+        <div className="content-container">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/about" component={DetailPage} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
