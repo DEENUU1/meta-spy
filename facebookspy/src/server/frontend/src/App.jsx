ee0693a6bf3ecb7ad78fb45bcd4a1a600f8e909f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-03 22:22:46 +0200

    Add HomePage, title short description and image (#142)

--- facebookspy/src/server/frontend/src/App.jsx
+++ facebookspy/src/server/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home';
+import HomePage from './pages/HomePage';
 import PersonDetail from './pages/PersonDetail';
 import PersonsPage from './pages/PersonsPage';
 
