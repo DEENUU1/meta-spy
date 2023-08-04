b4b3ea2f89430216b3f74fcb76c81f467f726ad8
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-05 00:34:20 +0200

    Add style for FriendCard (#155)

---
+++ facebookspy/src/server/frontend/src/Account/AccountDetails/components/FriendCard.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import LoadingDots from '../../Home/components/Loading';
+import '../styles/FriendCard.css'; 
+
+const FriendCard = ({ personId }) => {
+  const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/friend/${personId}`)
+      .then(response => {
+        setFriends(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, [personId]);
+
+  return (
+    <div className="friends-card">
+      <h2>Friends</h2>
+      {loading ? (
+        <LoadingDots />
+      ) : (
+        <ul className="friend-grid">
+          {friends.map(item => (
+            <li className="friend-card" key={item.id}> 
+              <strong>{item.full_name}</strong> <br />
+              <a href={item.url}>Facebook account</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default FriendCard;
