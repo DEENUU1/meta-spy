98809625be5b058afadea03ce74e3fc89631d7be
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-06 17:43:16 +0200

    Add url to person page in note modal (#160)

--- facebookspy/src/server/frontend/src/Note/ViewNoteModal.jsx
+++ facebookspy/src/server/frontend/src/Note/ViewNoteModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ModalContainer.css';
+import { Link } from 'react-router-dom';
 
 const ViewNoteModal = ({ note, onClose, setShowUpdateNoteModal }) => { 
   const handleUpdateNote = () => {
@@ -13,8 +14,8 @@ const ViewNoteModal = ({ note, onClose, setShowUpdateNoteModal }) => {
         <div className="modal-content">
           <h2>Note</h2>
           <p>{note.content}</p>
+          <Link to={`/person/${note.person_id}`}>Browse account</Link>
           <div className="modal-buttons">
-            <button onClick={handleUpdateNote}>Update</button>
             <button onClick={onClose}>Close</button>
           </div>
         </div>
