756ec3577c175e5dc5ad9c486ab4693c91846789
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-06 21:36:35 +0200

    Add pytest for friends and places endpoint (#138)

--- facebookspy/src/tests/test_endpoints.py
+++ facebookspy/src/tests/test_endpoints.py
@@ -1,9 +1,11 @@
+from typing import List
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from ..server.backend.app import app
 from ..database import get_session
-from ..models import Person, Videos, Reviews
+from ..models import Person, Videos, Reviews, Friends, Places
 
 
 @pytest.fixture
@@ -116,4 +118,52 @@ def test_get_videos_by_person_id(client, session):
     assert response.status_code == 200
     assert response.json() == [
         {"id": 1, "url": "https://www.youtube.com/watch?v=abc123", "person_id": 1},
+    ]
+
+
+def test_get_places_by_person_id(client: TestClient, session: Session):
+    person = Person(
+        full_name="John Doe", url="https://example.com/john-doe", facebook_id="abc"
+    )
+    place = Places(
+        name="Place A",
+        date="Some date",
+        person=person,
+    )
+    session.add(person)
+    session.add(place)
+    session.commit()
+
+    response = client.get("/person/place/1")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 1,
+            "name": "Place A",
+            "date": "Some date",
+            "person_id": 1,
+        }
+    ]
+
+
+def test_get_friends_by_person_id(client: TestClient, session: Session):
+    person = Person(
+        full_name="John Doe", url="https://example.com/john-doe", facebook_id="abc"
+    )
+    friend = Friends(
+        full_name="John Toe", url="https://example.com/john-toe", person=person
+    )
+    session.add(person)
+    session.add(friend)
+    session.commit()
+
+    response = client.get("/person/friend/1")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 1,
+            "full_name": "John Toe",
+            "url": "https://example.com/john-toe",
+            "person_id": 1,
+        }
     ]
