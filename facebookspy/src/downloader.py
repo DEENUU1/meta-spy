68370138edff93592881f5d83a856b073b177fa9
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-12 00:06:53 +0200

    Add method to download video using youtube dl lib and method to download videos for specified person from database (#180)

--- facebookspy/src/downloader.py
+++ facebookspy/src/downloader.py
@@ -7,7 +7,7 @@ import random
 import string
 from .logs import Logs
 from rich import print as rprint
-
+from .repository import get_person
 
 logs = Logs("downloader.py")
 
@@ -17,16 +17,40 @@ class Downloader:
     Download videos from facebook
     """
 
+    def __init__(self, person_facebook_id: str, video_url: str) -> None:
+        self.person_facebook_id = person_facebook_id
+        self.video_url = video_url
+
     @staticmethod
     def generate_random_video_title() -> str:
         """Generate random video title"""
         chars = string.ascii_letters
         return "".join(random.choice(chars) for _ in range(12))
 
-    def download_person_video(self):
-        """Download videos/reels from specified account"""
-        pass
+    @staticmethod
+    def download_video(path: str, video_url: str):
+        try:
+            ydl_opts = {"outtmpl": os.path.join(path)}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([video_url])
+        except Exception as e:
+            logs.log_error(f"An Error occurred while downloading videos: {e}")
 
-    def download_single_video(self):
+    def save_person_video(self):
+        """Download videos/reels from specified account"""
+        video_path = Config.VIDEO_PATH
+        if not os.path.exists(video_path):
+            os.makedirs(video_path)
+
+        person_video_path = os.path.dirname(f"{video_path}/{self.person_facebook_id}/")
+        if not os.path.exists(person_video_path):
+            os.makedirs(person_video_path)
+
+        video_filename = self.generate_random_video_title()
+        video_full_path = os.path.join(person_video_path, video_filename)
+
+        self.download_video(video_full_path, self.video_url)
+
+    def save_single_video(self):
         """Download single video/reel just by passing url"""
         pass
