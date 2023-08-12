694ca0e176ee824e85fa9643d33246ab13d9ace8
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-13 00:12:19 +0200

    Add function to check if review  object exists (#182)

--- facebookspy/src/repository.py
+++ facebookspy/src/repository.py
@@ -278,6 +278,16 @@ def get_new_videos(person_id: int) -> List[Videos]:
     )
 
 
+def review_exists(company: str, review: str, person_id: int) -> bool:
+    session = get_session()
+    review = (
+        session.query(Reviews)
+        .filter_by(company=company, review=review, person_id=person_id)
+        .first()
+    )
+    return review is not None
+
+
 def create_reviews(company: str, review: str, person_id: int) -> Reviews:
     session = get_session()
     reviews = Reviews(company=company, review=review, person_id=person_id)
