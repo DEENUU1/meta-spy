b2bf8e98ab13f7fd85ce4df2d61c4fffd2ac5ee1
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-13 14:27:06 +0200

    Set unlimited max overflow (#182)

--- facebookspy/src/database.py
+++ facebookspy/src/database.py
@@ -10,7 +10,7 @@ if docker_container == "true":
 else:
     db_path = "sqlite:///database.db"
 
-engine = create_engine(db_path)
+engine = create_engine(db_path, max_overflow=-1)
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 
