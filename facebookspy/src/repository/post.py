2bc7e96b7a96c90649d80f2c46fd6be23a647361
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-14 21:51:51 +0200

    Add function to create Post object (#214)

--- facebookspy/src/repository/post.py
+++ facebookspy/src/repository/post.py
@@ -1,4 +1,4 @@
-from ..models import Person, Posts
+from ..models import Person, Posts, PostSource
 
 from ..database import get_session
 from typing import List, Optional
@@ -9,3 +9,28 @@ def post_exists(url: str) -> bool:
     session = get_session()
     posts = session.query(Posts).filter_by(url=url).first()
     return posts is not None
+
+
+def create_post(
+    url: str,
+    person_id: int,
+    content: str,
+    number_of_likes: int,
+    number_of_shares: int,
+    number_of_comments: int,
+    source: PostSource,
+) -> Posts:
+    """Create Post object"""
+    session = get_session()
+    post = Posts(
+        url=url,
+        person_id=person_id,
+        content=content,
+        number_of_likes=number_of_likes,
+        number_of_shares=number_of_shares,
+        number_of_comments=number_of_comments,
+        source=source,
+    )
+    session.add(post)
+    session.commit()
+    return post
