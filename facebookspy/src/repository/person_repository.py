34cf91a383e5ce2dbc0bee8b77c679c16b1f388e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-23 16:57:21 +0200

    Update repository function for creating/updating Person object (#246)

--- facebookspy/src/repository/person_repository.py
+++ facebookspy/src/repository/person_repository.py
@@ -35,11 +35,15 @@ def get_person(facebook_id: str) -> Optional[Person]:
     return person
 
 
-def create_person(facebook_id: str, full_name=None) -> Person:
+def create_person(
+    facebook_id: str, full_name=None, phone_number=None, email=None
+) -> Person:
     """Create Person object
     Args:
         facebook_id (str): Facebook ID
         full_name (str): Full name
+        phone_number (str): Phone number
+        email (str): Email address
 
     Returns:
         Person: Person object.
@@ -52,8 +56,13 @@ def create_person(facebook_id: str, full_name=None) -> Person:
         person = Person(full_name=full_name, url=url, facebook_id=facebook_id)
         session.add(person)
         session.commit()
-    elif full_name is not None and person.full_name is None:
-        person.full_name = full_name
+    else:
+        if full_name is not None and person.full_name is None:
+            person.full_name = full_name
+        if phone_number is not None:
+            person.phone_number = phone_number
+        if email is not None:
+            person.email = email
         session.commit()
 
     return person
