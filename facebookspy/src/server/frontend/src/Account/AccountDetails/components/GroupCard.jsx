55ca1fb49c75341ab2b15984c4cd3ea573762f1f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-24 17:03:53 +0200

    Update endpoints urls (#250)

--- facebookspy/src/server/frontend/src/Account/AccountDetails/components/GroupCard.jsx
+++ facebookspy/src/server/frontend/src/Account/AccountDetails/components/GroupCard.jsx
@@ -7,7 +7,7 @@ const GroupCard = ({ personId }) => {
   const [loading, setLoading] = useState(true);  
 
   useEffect(() => {
-    axios.get(`http://localhost:8000/person/event/${personId}`)
+    axios.get(`http://localhost:8000/group/${personId}`)
       .then(response => {
         setGroups(response.data);
         setLoading(false);
