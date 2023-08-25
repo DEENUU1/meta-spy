76ee3e77672745c49b92b89c1679f3dde32a30a3
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-25 17:28:04 +0200

    add displaying scraped data in AccountFriend pipeline (#276) (#275) and display info about no data

--- facebookspy/src/facebook/account/account_friend.py
+++ facebookspy/src/facebook/account/account_friend.py
@@ -7,6 +7,8 @@ from ..facebook_base import BaseFacebookScraper
 from ..scroll import scroll_page
 from ...logs import Logs
 from ...repository import person_repository, friend_repository
+from ...cli import output
+
 
 logs = Logs()
 
@@ -64,39 +66,45 @@ class AccountFriend(BaseFacebookScraper):
 
             rprint("[bold]Step 3 of 3 - Extracting friends data[/bold]")
             extracted_data = self.extract_friends_data()
-            rprint(extracted_data)
 
-            rprint(
-                "[bold red]Don't close the app![/bold red] Saving scraped data to database, it can take a while!"
-            )
-
-            if not person_repository.person_exists(self._user_id):
-                person_repository.create_person(self._user_id)
-
-            person_id = person_repository.get_person(self._user_id).id
-
-            for data in extracted_data:
-                if not friend_repository.friend_exists(
-                    person_id, data["username"], data["url"]
-                ):
-                    friend_repository.create_friends(
-                        data["username"], data["url"], person_id
-                    )
-
-            # Update the number of friends in the person table
-            number_of_person_friends = friend_repository.get_number_of_friends(
-                person_id
-            )
-            person_repository.update_number_of_friends(
-                person_id, number_of_person_friends
-            )
-            if person_repository:
-                rprint("[bold green]Person table updated[/bold green]")
+            if not any(extracted_data):
+                output.print_no_data_info()
+                self._driver.quit()
+                self.success = False
             else:
-                rprint("[bold red]Person table not updated[/bold red]")
+                output.print_data_from_list_of_dict(extracted_data)
 
-            self._driver.quit()
-            self.success = True
+                rprint(
+                    "[bold red]Don't close the app![/bold red] Saving scraped data to database, it can take a while!"
+                )
+
+                if not person_repository.person_exists(self._user_id):
+                    person_repository.create_person(self._user_id)
+
+                person_id = person_repository.get_person(self._user_id).id
+
+                for data in extracted_data:
+                    if not friend_repository.friend_exists(
+                        person_id, data["username"], data["url"]
+                    ):
+                        friend_repository.create_friends(
+                            data["username"], data["url"], person_id
+                        )
+
+                # Update the number of friends in the person table
+                number_of_person_friends = friend_repository.get_number_of_friends(
+                    person_id
+                )
+                person_repository.update_number_of_friends(
+                    person_id, number_of_person_friends
+                )
+                if person_repository:
+                    rprint("[bold green]Person table updated[/bold green]")
+                else:
+                    rprint("[bold red]Person table not updated[/bold red]")
+
+                self._driver.quit()
+                self.success = True
 
         except Exception as e:
             logs.log_error(f"An error occurred: {e}")
