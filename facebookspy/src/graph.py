8aee6eafba52072d6fae94d5711319a0b138c73d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-27 01:35:28 +0200

    update function to create a graph of Person object based on friends (#281)

--- facebookspy/src/graph.py
+++ facebookspy/src/graph.py
@@ -7,17 +7,23 @@ persons = person_repository.get_persons()
 
 
 def create_relationship_graph():
-    graph = nx.DiGraph()
+    G = nx.DiGraph()
 
     for person in persons:
-        graph.add_node(person.id, label=person.full_name)
+        G.add_node(person.id, label=person.full_name)
 
-        for friend in person.friends:
-            graph.add_edge(person.id, friend.person_id)
+    for person in persons:
+        friend_urls = set(friend.url for friend in person.friends)
+        for other_person in persons:
+            if person != other_person:
+                other_friend_urls = set(friend.url for friend in other_person.friends)
+                common_urls = friend_urls.intersection(other_friend_urls)
+                if common_urls:
+                    G.add_edge(person.id, other_person.id)
 
-    pos = nx.spring_layout(graph)
-    labels = nx.get_node_attributes(graph, "label")
-    nx.draw(graph, pos, labels=labels, with_labels=True, node_size=1000)
+    pos = nx.spring_layout(G)
+    labels = nx.get_node_attributes(G, "label")
+    nx.draw(G, pos, labels=labels, with_labels=True, node_size=1000)
     plt.show()
 
-    return graph
+    return G
