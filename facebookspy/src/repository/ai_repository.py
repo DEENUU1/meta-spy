438536c8f70d73d2519c2ee780ff3b91e3a839be
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-03 01:27:57 +0200

    Add function to create or update Ai object for specified Person object (#298)

--- facebookspy/src/repository/ai_repository.py
+++ facebookspy/src/repository/ai_repository.py
@@ -1,4 +1,5 @@
 from ..models import Ai
+from ..database import get_session
 
 
 def ai_exists(person_id: int) -> bool:
@@ -10,4 +11,32 @@ def ai_exists(person_id: int) -> bool:
     Returns:
         bool: True if ai exists, False otherwise
     """
-    return Ai.query.filter_by(person_id=person_id).first() is not None
+    session = get_session()
+    return session.query(Ai).filter_by(person_id=person_id).first() is not None
+
+
+def create_or_update_ai(person_id, person_summary=None, person_opinion=None) -> Ai:
+    """Create or update Ai object for specified Person object
+
+    Args:
+        person_id (int): PersonId
+        person_summary (str): PersonSummary
+        person_opinion (str): PersonOpinion
+    """
+    session = get_session()
+    ai = session.query(Ai).filter_by(person_id=person_id).first()
+
+    if ai:
+        if person_summary is not None:
+            ai.person_summary = person_summary
+        if person_opinion is not None:
+            ai.person_opinion = person_opinion
+    else:
+        ai = Ai(
+            person_summary=person_summary,
+            person_opinion=person_opinion,
+            person_id=person_id,
+        )
+
+    session.add(ai)
+    session.commit()
