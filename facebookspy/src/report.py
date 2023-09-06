57a9bcac95f7f58a0a31939ecfcc1a3dc39af61e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-07 00:50:06 +0200

    Add custom font (#295)

--- facebookspy/src/report.py
+++ facebookspy/src/report.py
@@ -5,17 +5,24 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
 from reportlab.platypus.tables import Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import inch
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 from rich import print as rprint
 
 
 def generate_pdf_report(person_id: str) -> None:
     """
-    Generate and save PDF file with scraped data for specified person.
+    Generate and save PDF file with scraped data for the specified person.
     """
 
-    rprint(f"[bold green]Start generating report for person: {person_id}[/bold green]")
+    rprint(
+        f"[bold green]Start generating a report for person: {person_id}[/bold green]"
+    )
     rprint("[bold]Step 1 of 3 - Extract data from models[/bold]")
     data = format_person_data(person_id)
+
+    pdfmetrics.registerFont(TTFont("Basica-UnicodeRegular", "font.ttf"))
+
     doc = SimpleDocTemplate(f"{person_id}_report.pdf", pagesize=letter)
     story = []
 
@@ -29,7 +36,7 @@ def generate_pdf_report(person_id: str) -> None:
         f"Phone Number: {data[0].phone_number}",
     ]
 
-    rprint("[bold]Step 2 of 3 - Adding data to PDF file [/bold]")
+    rprint("[bold]Step 2 of 3 - Adding data to the PDF file [/bold]")
     main_info_text = "\n".join(person_personal_info)
     story.append(Paragraph("Main User Information", heading_style))
     story.append(Spacer(1, 0.2 * inch))
@@ -53,5 +60,5 @@ def generate_pdf_report(person_id: str) -> None:
         story.append(Paragraph(content, styles["Normal"]))
         story.append(PageBreak())
 
-    rprint("[bold]Step 3 of 3 - Saving PDF file[/bold]")
+    rprint("[bold]Step 3 of 3 - Saving the PDF file[/bold]")
     doc.build(story)
