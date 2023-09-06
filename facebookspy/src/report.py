c7d481b32271146bd41ce57e5ce5750e10222345
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-07 00:28:18 +0200

    Add template of a function and command to generate report for specified Person (#295)

---
+++ facebookspy/src/report.py
@@ -0,0 +1,8 @@
+from .ai import format_person_data
+
+
+def generate_pdf_report(person_id: str) -> None:
+    """
+    Generate and save PDF file with scraped data for specified person.
+    """
+    pass
