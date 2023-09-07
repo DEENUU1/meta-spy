f5de12248e99fc319cb06651511ac0d07f3a9d9b
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-09-07 15:46:22 +0200

    Add commands in generate_pdf_report (#295)

--- facebookspy/src/report.py
+++ facebookspy/src/report.py
@@ -1,8 +1,6 @@
 from .ai import format_person_data
-from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
-from reportlab.platypus.tables import Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import inch
 from reportlab.pdfbase import pdfmetrics
@@ -19,8 +17,11 @@ def generate_pdf_report(person_id: str) -> None:
         f"[bold green]Start generating a report for person: {person_id}[/bold green]"
     )
     rprint("[bold]Step 1 of 3 - Extract data from models[/bold]")
+
+    # Get formatet data
     data = format_person_data(person_id)
 
+    # Add custom font to the PDF file
     pdfmetrics.registerFont(TTFont("Basica-UnicodeRegular", "font.ttf"))
 
     doc = SimpleDocTemplate(f"{person_id}_report.pdf", pagesize=letter)
@@ -37,6 +38,8 @@ def generate_pdf_report(person_id: str) -> None:
     ]
 
     rprint("[bold]Step 2 of 3 - Adding data to the PDF file [/bold]")
+
+    # Implement data to the PDF file
     main_info_text = "<br/>".join(person_personal_info)
     story.append(Paragraph("Main User Information", heading_style))
     story.append(Spacer(1, 0.2 * inch))
@@ -55,13 +58,12 @@ def generate_pdf_report(person_id: str) -> None:
         "Work and Education",
     ]
     for heading, content in zip(headings, data[1:]):
-        content_with_line_breaks = "<br/>".join(
-            content.split("\n")
-        )  # Zamiana \n na <br/> w treści
+        content_with_line_breaks = "<br/>".join(content.split("\n"))
         story.append(Paragraph(heading, heading_style))
         story.append(Spacer(1, 0.2 * inch))
         story.append(Paragraph(content_with_line_breaks, styles["Normal"]))
-        story.append(PageBreak())
 
     rprint("[bold]Step 3 of 3 - Saving the PDF file[/bold]")
+
+    # Save the PDF file
     doc.build(story)
