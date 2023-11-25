3ea3f1554079b5a7d5a7550c0667c06eb09e02e8
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-11-25 10:11:04 +0100

    Update create_image repository function (#373)

--- metaspy/src/repository/instagram_image_repository.py
+++ metaspy/src/repository/instagram_image_repository.py
@@ -9,25 +9,18 @@ def image_exists(url: str) -> bool:
     return image is not None
 
 
-def create_image(url: str, account_id: int) -> InstagramImages:
+def create_image(
+    url: str, account_id: int, number_of_likes: int = 0
+) -> InstagramImages:
     session = get_session()
-    image = InstagramImages(url=url, account_id=account_id)
+    image = InstagramImages(
+        url=url, account_id=account_id, number_of_likes=number_of_likes
+    )
     session.add(image)
     session.commit()
     return image
 
 
-def set_as_downloaded(image: InstagramImages) -> None:
-    image.downloaded = True
-    get_session().commit()
-    return
-
-
 def get_all() -> Optional[List[InstagramImages]]:
     session = get_session()
     return session.query(InstagramImages).all()
-
-
-def get_not_downloaded() -> Optional[List[InstagramImages]]:
-    session = get_session()
-    return session.query(InstagramImages).filter_by(downloaded=False).all()
